2f6cf35f77e08d5091af2f8fd944494148021ea4
ldx027 <17301339186@163.com>
2024-03-18 10:51:25 +0800

    2024-03-18

---
+++ luogu/p1102_A-B数对.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> vec;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N, C;
+    cin >> N >> C;
+
+    vec.resize(N);
+    for (int i = 0; i < N; i++) cin >> vec[i];
+    sort(vec.begin(), vec.end());
+
+    int ans = 0;
+    for (int i = 0; i < N; i++)
+    {
+        int lower = lower_bound(vec.begin(), vec.end(), vec[i] - C) - vec.begin();
+        int upper = upper_bound(vec.begin() + lower, vec.end(), vec[i] - C) - vec.begin();
+        ans += upper - lower;
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
