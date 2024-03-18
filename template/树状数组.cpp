1ae7a51ec5f495976d4cdbcb5cf5e013667d730b
ldx027 <17301339186@163.com>
2024-03-18 15:32:17 +0800

    去你妈的吧谁爱学谁学吧

---
+++ template/树状数组.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+inline int lowbit(int x) { return x & -x; }
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
