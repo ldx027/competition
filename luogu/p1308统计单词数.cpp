94a4cdd0858a062c101668055b258ce61e6d686a
ldx027 <17301339186@163.com>
2024-03-18 20:28:56 +0800

    change p1308

--- luogu/p1308统计单词数.cpp
+++ luogu/p1308统计单词数.cpp
@@ -2,9 +2,36 @@
 #include <string>
 using namespace std;
 
+void change(string &str)
+{
+    for (auto it = str.begin(); it != str.end(); it++)
+    {
+        if (*it >= 'A' && *it <= 'Z') *it += 'a' - 'A';
+    }
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+
+    string find, input;
+    getline(cin, find);
+    getline(cin, input);
+    change(find);
+    change(input);
+
+    find += ' ';
+
+    int cnt = 0;
+    int first = -1;
+
+    for (int i = 0; i < input.size() - find.size(); i++)
+    {
+        for (int j = 0; j < find.size(); j++) if (find[j] != input[i + j]) continue;
+        cnt++;
+        if (first == -1) first = i;
+        i += find.size();
+    }
 }
