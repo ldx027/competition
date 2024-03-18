80386b74ce71c71d9cf013d69fe267194cb20c3a
ldx027 <17301339186@163.com>
2024-03-18 20:38:19 +0800

    complete p1308

--- luogu/p1308统计单词数.cpp
+++ luogu/p1308统计单词数.cpp
@@ -6,7 +6,8 @@ void change(string &str)
 {
     for (auto it = str.begin(); it != str.end(); it++)
     {
-        if (*it >= 'A' && *it <= 'Z') *it += 'a' - 'A';
+        if (*it >= 'A' && *it <= 'Z')
+            *it += 'a' - 'A';
     }
 }
 
@@ -23,15 +24,34 @@ int main()
     change(input);
 
     find += ' ';
+    input += ' ';
 
     int cnt = 0;
     int first = -1;
 
-    for (int i = 0; i < input.size() - find.size(); i++)
+    for (int i = 0; i < input.size() - find.size();)
     {
-        for (int j = 0; j < find.size(); j++) if (find[j] != input[i + j]) continue;
-        cnt++;
-        if (first == -1) first = i;
-        i += find.size();
+        bool flg = true;
+        for (int j = 0; j < find.size(); j++)
+            if (find[j] != input[i + j])
+            {
+                flg = false;
+                break;
+            }
+        if (flg)
+        {
+            cnt++;
+            if (first == -1)
+                first = i;
+        }
+        if (input[i] != ' ') while(input[i] != ' ') i++;
+        while (input[i] == ' ') i++;
     }
+
+    if (cnt)
+        cout << cnt << " " << first << endl;
+    else
+        cout << -1 << endl;
+
+    return 0;
 }
