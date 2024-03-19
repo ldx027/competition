0b009843188bd0c7051831f20fd40faf2290f887
ldx027 <17301339186@163.com>
2024-03-19 10:33:08 +0800

    change temoplate 树状数组.cpp

--- template/树状数组.cpp
+++ template/树状数组.cpp
@@ -2,11 +2,38 @@
 #include <vector>
 using namespace std;
 
-inline int lowbit(int x) { return x & -x; }
+vector<int> vec;
+
+int lowbit(int x) { return x & -x; }
+
+void add(int idx, int k)
+{
+    while (idx < vec.size())
+    {
+        vec[idx] += k;
+        idx += lowbit(idx);
+    }
+}
+
+int sum(int idx)
+{
+    int rst = 0;
+    while (idx > 0)
+    {
+        rst += vec[idx];
+        idx -= lowbit(idx);
+    }
+
+    return rst;
+}
 
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
+    
+    int N, M;
+    cin >> N >> M;
+    vec.resize(N, 0);
 }
