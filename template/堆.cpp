e4e802d4d450d6f948d12ff9fac648d65166d841
ldx027 <17301339186@163.com>
2024-03-19 13:19:42 +0800

    add template heap

---
+++ template/堆.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> heap;
+
+void swap(int a, int b)
+{
+    if (a == b) return;
+    int temp = heap[a];
+    heap[a] = heap[b];
+    heap[b] = temp;
+}
+
+void push(int x)
+{
+    heap.push_back(x);
+    int idx = heap.size() - 1;
+    while (heap[idx] > heap[(idx - 1) / 2])
+    {
+        swap(idx, (idx - 1) / 2);
+        idx = (idx - 1) / 2;
+    }
+}
+
+void pop()
+{
+    swap(0, heap.size() - 1);
+    heap.pop_back();
+    int idx = 0;
+
+    while(1)
+    {
+        if (idx * 2 + 1 >= heap.size()) break;
+        int largest = heap[idx] > heap[idx * 2 + 1] ? idx : (idx * 2 + 1);
+        largest = ((idx * 2 + 2) < heap.size() && heap[idx * 2 + 2] > heap[largest]) ? (idx * 2 + 2) : largest;
+        if (idx == largest) break;
+        swap(idx, largest);
+        idx = largest;
+    }
+}
+
+void show()
+{
+    for (int i = 0; i < heap.size(); i++) cout << heap[i] << " ";
+    cout << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    int op, x;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> op;
+        if (op == 1)
+        {
+            cin >> x;
+            push(x);
+        }
+        else if (op == 2)
+        {
+            cout << heap[0] << endl;
+        }
+        else if (op == 3)
+        {
+            pop();
+        }
+    }
+
+    return 0;
+}
