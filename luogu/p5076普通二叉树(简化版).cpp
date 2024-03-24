e3ed4320387e43ed969619855d98119a5cd8b430
ldx027 <17301339186@163.com>
2024-03-24 15:27:10 +0800

    fix .gitignore

--- luogu/p5076普通二叉树(简化版).cpp
+++ luogu/p5076普通二叉树(简化版).cpp
@@ -1,107 +1,17 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 using namespace std;
-
-#define inf 2147483647
 
 struct Node
 {
     int val;
-    int num;
-    Node *pre;
-    Node *left;
-    Node *right;
-    Node(int _val) : val(_val)
-    {
-        this->num = 1;
-        this->left = nullptr;
-        this->right = nullptr;
-    }
+    int left, right;
+    int cnt;
+    int siz;
 };
 
-void insert(Node *head, int x)
-{
-    Node *cur = head;
-    while (cur->val > x && cur->left != nullptr || cur->val < x && cur->right != nullptr)
-    {
-        if (cur->val > x)
-            cur = cur->left;
-        else if (cur->val < x)
-            cur = cur->right;
-    }
-    if (cur->val == x)
-    {
-        cur->num++;
-    }
-    else if (cur->val < x)
-    {
-        cur->right = new Node(x);
-        cur->right->pre = cur;
-    }
-    else if (cur->val > x)
-    {
-        cur->left = new Node(x);
-        cur->left->pre = cur;
-    }
-}
-
-int rank(Node *node)
-{
-    if (node->left == nullptr) return 1;
-    Node *cur = node->left;
-    queue<Node*> que;
-    que.push(cur);
-    
-    int rst = 1;
-    while (!que.empty())
-    {
-        cur = que.front();
-        que.pop();
-        rst += cur->num;
-        if (cur->left != nullptr) que.push(cur->left);
-        if (cur->right != nullptr) que.push(cur->right);
-    }
-
-    return rst;
-}
-
-int fr(Node *head, int x)
-{
-    Node *cur = head;
-    while (cur->val >= x)
-    {
-        if (cur->left == nullptr) return -inf;
-        cur = cur->left;
-    }
-
-    while (cur->right != nullptr) cur = cur->right;
-    
-    return cur->val;
-}
-
-int ne(Node *head, int x)
-{
-    Node *cur = head;
-    while (cur->val <= x)
-    {
-        if (cur->right == nullptr) return inf;
-        cur = cur->right;
-    }
-
-    while (cur->left != nullptr) cur = cur->left;
-    
-    return cur->val;
-}
-
-int xRank(Node *head, int x)
-{
-    Node *cur = head;
-    while (cur->num)
-}
+vector<Node> tree;
 
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
 }
