6d1343ae2c6c2359f153baf6be8ee33ac7547b08
ldx027 <17301339186@163.com>
2024-04-09 19:34:09 +0800

    feat p2866

--- luogu/p2866_Bad_Hair_Day_S.cpp
+++ luogu/p2866_Bad_Hair_Day_S.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
-#include <vector>
+#include <stack>
 using namespace std;
+
+#define ll long long
 
 int main()
 {
@@ -8,6 +10,19 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    int N;
+    ll N;
     cin >> N;
+
+    ll ans = 0, x;
+    stack<ll> stk;
+    for (ll i = 0; i < N; i++)
+    {
+        cin >> x;
+        while (stk.size() && x >= stk.top()) stk.pop();
+        ans += stk.size();
+        stk.push(x);
+    }
+    cout << ans << endl;
+
+    return 0;
 }
