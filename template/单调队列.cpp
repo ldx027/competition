ac6913b8ec886cac7377920eb11a1dbe34146e81
ldx027 <17301339186@163.com>
2024-04-10 16:46:30 +0800

    change template

--- template/单调队列.cpp
+++ template/单调队列.cpp
@@ -1,35 +1,7 @@
 #include <iostream>
-#include <list>
+#include <queue>
 #include <utility>
 using namespace std;
-
-struct MonoQue
-{
-    list<pair<int, int>> lst;
-    bool (*cmp)(int, int);
-    int cnt = 0;
-
-    MonoQue(bool (*_cmp)(int, int)) : cmp(_cmp) {}
-
-    void push(int x, int idx)
-    {
-        while (lst.size() > 0 && cmp(lst.rbegin()->first, x))
-            lst.pop_back();
-        lst.push_back({x, idx});
-    }
-
-    void slide()
-    {
-        cnt++;
-        while (lst.size() > 0 && lst.begin()->second < cnt)
-            lst.pop_front();
-    }
-
-    int front() { return lst.begin()->first; }
-};
-
-bool bigger(int x, int y) { return x > y; }
-bool smaller(int x, int y) { return x < y; }
 
 int main()
 {
@@ -37,43 +9,19 @@ int main()
     cin.tie(nullptr);
     cout.tie(nullptr);
 
-    MonoQue biggerQue(bigger);
-    MonoQue smallerQue(smaller);
+    deque<pair<int, int>> que;
 
-    list<int> rst1, rst2;
-
-    int n, k;
+    int n, k, x;
     cin >> n >> k;
 
-    int idx = 0;
-    int x;
-    while (idx < k)
+    for (int i = 0; i < n; i++)
     {
         cin >> x;
-        biggerQue.push(x, idx);
-        smallerQue.push(x, idx);
-        idx++;
+        while (que.size() && que.front().second <= i - k) que.pop_front();
+        while (que.size() && que.back().first < x) que.pop_back();
+        que.push_back({x, i});
+        if (i >= k - 1) cout << que.front().first << " ";
     }
-
-    rst1.push_back(biggerQue.front());
-    rst2.push_back(smallerQue.front());
-
-    while (idx < n)
-    {
-        cin >> x;
-        biggerQue.push(x, idx);
-        smallerQue.push(x, idx);
-        biggerQue.slide();
-        smallerQue.slide();
-        rst1.push_back(biggerQue.front());
-        rst2.push_back(smallerQue.front());
-        idx++;
-    }
-
-    for (auto it = rst1.begin(); it != rst1.end(); it++) cout << *it << " ";
-    cout << endl;
-    for (auto it = rst2.begin(); it != rst2.end(); it++) cout << *it << " ";
-    cout << endl;
 
     return 0;
 }
