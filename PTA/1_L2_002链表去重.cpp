24c1c7591138c9957c7db963596dc2b082d3352e
ldx027 <17301339186@163.com>
2024-04-17 18:55:54 +0800

    new file:   1_L2_001紧急救援.cpp 	new file:   1_L2_002链表去重.cpp

---
+++ PTA/1_L2_002链表去重.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Node
+{
+    int adrs;
+    int val;
+    int next;
+};
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int L, N;
+    cin >> L >> N;
+    vector<Node> mem(1e6 + 10);
+}
